41e0dc48552797ce7486a8d34ddb549a5a7aface
hiro4869 <sakuragima48@gmail.com>
2017-06-08 18:25:56 +0900

    サロン参加者のみ閲覧できるテスト成功

--- app/controllers/owner_controller.rb
+++ app/controllers/owner_controller.rb
@@ -1,12 +1,33 @@
 class OwnerController < ApplicationController
+  before_action :correct_salon_user, only: :show
+
   def salon_index
     @SalonApplying = SalonApplying.new
     @owner_id = params[:id]
   end
 
   def show
+    # @salon = SalonApplying.where(user_id: "#{current_user.id}").where(owner_id: "#{params[:id]}")
+    mysalon = SalonApplying.where(user_id: "#{current_user.id}").where(owner_id: "6")
+    if mysalon.exists?
+      @answer = "あるよ！"
+    else
+      @answer = "ないよ！"
+    end
+
+
+
   end
 
   def owner
   end
+
+  private
+
+  def correct_salon_user
+    #サロンに参加していない人はサロンのトップページにリダイレクトさせる
+    mysalon = SalonApplying.where(user_id: "#{current_user.id}").where(owner_id: "#{params[:id]}")
+    redirect_to salon_index_owner_path(params[:id]) unless mysalon.exists?
+  end
+
 end
