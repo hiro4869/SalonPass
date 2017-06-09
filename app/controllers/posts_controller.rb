292100f63785874a37fed06afa27817b28d5608c
hiro4869 <sakuragima48@gmail.com>
2017-06-09 15:02:58 +0900

    サロンのメンバーだけがサロン掲示板にアクセス可能にする機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
+  before_action :correct_salon_user, only: :index
 
   def index
-    @salon_id = params[:owner_id]
+    @salon_id = params[:salon_id]
   end
 
+
+  private
+
+  def correct_salon_user
+    #サロンに参加していない人はサロンのトップページにリダイレクトさせる
+    if current_user.nil?
+      redirect_to root_path unless current_owner.id == "#{params[:salon_id]}".to_i
+    else
+      mysalon = SalonApproved.where(user_id: "#{current_user.id}").where(owner_id: "#{params[:salon_id]}")
+      redirect_to salon_index_salon_path(params[:salon_id]) unless mysalon.exists?
+    end
+  end
 
 end
