f94e2b58f5dae808d4b3e476ad4830723d960716
hiro4869 <sakuragima48@gmail.com>
2017-06-17 14:28:24 +0900

    購入確認画面に合計機能を実装

--- app/models/shopcart.rb
+++ app/models/shopcart.rb
@@ -1,2 +1,16 @@
 class Shopcart < ApplicationRecord
+  belongs_to :product
+
+
+  def subtotal
+    self.product.price * self.purchase_num
+  end
+
+  # def total
+  #     total = 0
+  #   self.each do |item|
+  #     total += item.subtotal
+  #   end
+  # end
+
 end
