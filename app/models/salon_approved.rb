13140868a87b048d594a5e35f259f2c495b09fa9
hiro4869 <sakuragima48@gmail.com>
2017-07-02 17:55:41 +0900

    違うサロン名が表示されるバグを解消

--- app/models/salon_approved.rb
+++ app/models/salon_approved.rb
@@ -1,6 +1,7 @@
 class SalonApproved < ApplicationRecord
   # belongs_to :owner
   belongs_to :salon_information, foreign_key: "salon_id", optional: true
+
   belongs_to :user, optional: true
 
 end
--- app/models/salon_information.rb
+++ app/models/salon_information.rb
@@ -1,6 +1,10 @@
 class SalonInformation < ApplicationRecord
+  self.primary_key = "salon_id"
+
   belongs_to :owner ,optional: true
-  belongs_to :salon_approved, foreign_key: "salon_id", optional: true
+  has_many :salon_approveds
+
+
   belongs_to :salon_applying, foreign_key: "salon_id", optional: true
 
   validates :salon_id, uniqueness: true
